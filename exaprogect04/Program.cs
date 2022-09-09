fea16f16a64b56bcd59349825aeb464ce4ac7d68
manjahha <manjahha@gmail.com>
2022-09-09 13:51:04 +0300

    greetings1

---
+++ exaprogect04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("enter you name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Ben")
+
+{
+    Console.WriteLine("greetings, my lord");
+}
+else
+{
+    Console.Write("Hi");
+    Console.WriteLine(username);
+}
